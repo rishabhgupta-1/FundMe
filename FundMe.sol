a85cf24c92b05767126121d03ec4ef6f5a74e242
Rishabh Gupta <48594987+rishabhgupta-1@users.noreply.github.com>
2023-06-14 18:46:52 +0530

    conversionRate FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -5,9 +5,11 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 
 contract FundMe{
 
+    uint256 public minUsd = 5e18;
+
     function fund() public payable{
         //set a min fund value
-        require(msg.value > 1e18, "Didn't send enought!"); //1e18 = 1*10**18 = 1000000000000000000
+        require(getConversionRate(msg.value) >= minUsd, "Didn't send enough ETH!"); //1e18 = 1*10**18 = 1000000000000000000
     }
 
     function getPrice() public view returns(uint256) {
@@ -16,5 +18,10 @@ contract FundMe{
         (, int256 price, , , )  = priceFeed.latestRoundData();
         return uint256(price * 1e10);
     }
-    //function withdraw(){}
+    
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 }
