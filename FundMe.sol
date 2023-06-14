f7a9c63eca3cb11f93ca4976661363e44659f5ab
Rishabh Gupta <48594987+rishabhgupta-1@users.noreply.github.com>
2023-06-14 19:10:54 +0530

    Using PriceConverter Library FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,27 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
-contract FundMe{
+contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minUsd = 5e18;
 
+    address[] public funders; //keeping track of funders
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    //mapping address to amount, to keep track of amount send by particular sender
+
     function fund() public payable{
         //set a min fund value
-        require(getConversionRate(msg.value) >= minUsd, "Didn't send enough ETH!"); //1e18 = 1*10**18 = 1000000000000000000
-    }
-
-    function getPrice() public view returns(uint256) {
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price, , , )  = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-    
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+        require(msg.value.getConversionRate() >= minUsd, "Didn't send enough ETH!"); //1e18 = 1*10**18 = 1000000000000000000
+        funders.push(msg.sender); //adding sender address to funders
     }
 }
