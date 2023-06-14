fa01a55b460cee8f5af5ef48cf0971487f15de1a
Rishabh Gupta <48594987+rishabhgupta-1@users.noreply.github.com>
2023-06-14 18:11:47 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
 
     function fund() public payable{
-        //setting a min fund value
+        //set a min fund value
         require(msg.value > 1e18, "Didn't send enought!"); //1e18 = 1*10**18 = 1000000000000000000
+    }
 
+    function getPrice() public view returns(uint256) {
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , , )  = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
     }
     //function withdraw(){}
 }
